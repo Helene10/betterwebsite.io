dd14109637504ae8af0c9762423ac9b7fa1f4ef7
Hélène <helene.cbm@gmail.com>
2018-09-25 15:30:18 +0300

    avec java

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+if (confirm("Voulez vs aller sur le super site xxxvidsxxx?")) {
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+} else {
+    txt = "You pressed Cancel!";
+}
+
